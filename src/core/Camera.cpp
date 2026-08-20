0e102c61dc1084fc3d4db381e0d21b174ae6dad4
LeoLu1226 <1877991272@qq.com>
2026-08-20 09:56:02 +0800

    Camera: adjusted camera acc values
    Signed-off-by: LeoLu1226 <1877991272@qq.com>

--- src/core/Camera.cpp
+++ src/core/Camera.cpp
@@ -197,7 +197,7 @@ CCamera::Init(void)
 		m_bMusicFading = false;
 		m_fTimeToFadeMusic = 0.0f;
 		m_fFLOATingFadeMusic = 0.0f;
-		m_fMouseAccelVertical = 0.003f;
+		m_fMouseAccelVertical = 0.0025f;
 		m_fMouseAccelHorzntl = 0.0025f;
 	}
 	if(FrontEndMenuManager.m_bWantToRestart)
@@ -241,7 +241,7 @@ CCamera::Init(void)
 	m_uiTransitionState = 0;
 	m_uiTimeTransitionStart = 0;
 	m_bLookingAtPlayer = true;
-	m_f3rdPersonCHairMultX = 0.53f;
+	m_f3rdPersonCHairMultX = 0.4f;
 	m_f3rdPersonCHairMultY = 0.4f;
 	m_fAvoidTheGeometryProbsTimer = 0.0f;
 	m_nAvoidTheGeometryProbsDirn = 0;
