f398db99dfaa8d54039d9d0eefc77ce251fbf4f3
LeoLu1226 <1877991272@qq.com>
2026-08-20 17:58:25 +0800

    Change Frame Limiter from 30 to 60
    Signed-off-by: LeoLu1226 <1877991272@qq.com>

--- src/skel/skeleton.cpp
+++ src/skel/skeleton.cpp
@@ -403,7 +403,7 @@ RsInitialize(void)
 	RsGlobal.width = DEFAULT_SCREEN_WIDTH;
 	RsGlobal.height = DEFAULT_SCREEN_HEIGHT;
 	
-	RsGlobal.maxFPS = 30;
+	RsGlobal.maxFPS = 60;
 	 
 	RsGlobal.quit = FALSE;
 
